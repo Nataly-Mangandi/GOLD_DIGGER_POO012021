db31867c20a63346eb9007aea931d02198fffdac
Nataly-Mangandi <00108520@uca.edu.sv>
2021-03-12 00:04:48 -0600

    commit

---
+++ ContadoresAcumuladores/ContadoresYAcumuladores.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ContadoresAcumuladores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("// CONTADORES Y ACUMULADORES //");
+            Console.WriteLine(" ");
+
+            int n = 0, n2 = 0;
+
+            Console.Write("Ingrese un numero para iniciar: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            n2 = n-1;
+
+            for(int i = 0; i < n3; i++)
+            {
+                for(int j = 0; j < n2; j++)
+                {
+                    Console.Write($"{n-j} \t");
+                }
+                Console.WriteLine(" ");
+                n2--;
+            }
+            
+            
+
+        }
+    }
+}
